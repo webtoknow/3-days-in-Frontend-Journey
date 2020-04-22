eb7062e66c6bbb590415d9adeec510d6dcb777da
Bogdan <bmnicolae@gmail.com>
2020-04-22 16:09:57 +0300

    React  - Add/Edit and Delete articles

--- React-implementation/Exercise/Solution/travelling-blog/src/App.tsx
+++ React-implementation/Exercise/Solution/travelling-blog/src/App.tsx
@@ -1,64 +1,157 @@
 import React, { Component } from 'react';
+import axios, { AxiosResponse } from 'axios'
+interface Article {
+  title: string;
+  tag: string;
+  author: string;
+  date: string;
+  imgUrl: string;
+  content: string;
+}
 
-class App extends Component {
+interface ArticleFromServer extends Article {
+  id: number;
+}
+
+interface Props {
+}
+
+interface State extends ArticleFromServer {
+  openModal: boolean;
+  articles: ArticleFromServer[];
+}
+
+class App extends Component<Props, State>  {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      openModal: false,
+      articles: [],
+      id: 0,
+      title: '',
+      tag: '',
+      author: '',
+      date: '',
+      imgUrl: '',
+      content: '',
+    };
+
+  }
+  componentDidMount() {
+    this.getArticle();
+  }
+
+  getArticle() {
+    axios.get('http://localhost:3000/articles').then((response: AxiosResponse) => {
+      const articles: ArticleFromServer[] = response.data;
+      this.setState({ articles })
+    })
+  }
+
+  openModalOnAdd = () => {
+    this.setState({ openModal: true })
+  }
+
+  openModalOnEdit = (article: ArticleFromServer) => {
+    const { id, title, tag, author, date, imgUrl, content } = article;
+    this.setState({ id, title, tag, author, date, imgUrl, content, openModal: true })
+  }
+
+  closeModalAndResetArticleState = () => {
+    this.setState({
+      openModal: false,
+      id: 0,
+      title: '',
+      tag: '',
+      author: '',
+      date: '',
+      imgUrl: '',
+      content: '',
+    })
+  }
+
+  handleFieldChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    this.setState({
+      ...this.state,
+      [event.target.name]: event.target.value
+    });
+  };
+
+  saveArticle = () => {
+    const { id, title, tag, author, date, imgUrl, content } = this.state;
+    const articleForServer = { title, tag, author, date, imgUrl, content }
+    if (id !== 0) {
+      axios.put(`http://localhost:3000/articles/${id}`, articleForServer).then((response: AxiosResponse) => {
+        this.closeModalAndResetArticleState();
+        this.getArticle();
+      })
+    }
+    else {
+      axios.post(`http://localhost:3000/articles`, articleForServer).then((response: AxiosResponse) => {
+        this.closeModalAndResetArticleState();
+        this.getArticle();
+      })
+    }
+  }
+
+  deleteArticle = (id: number) => {
+    axios.delete(`http://localhost:3000/articles/${id}`).then((response: AxiosResponse) => {
+      this.getArticle();
+    })
+  }
+
   render() {
+    const openModalclassName = this.state.openModal ? 'show-modal' : '';
+    const articleList = this.state.articles.map((article: ArticleFromServer) => (
+      <article key={article.id}>
+        <h2 className="title">{article.title}</h2>
+        <ul className="info__container">
+          <li className="info__item">{article.tag}</li>
+          <li className="info__item">Added by &nbsp;
+                        <span className="info__mark">{article.author}</span>
+          </li>
+          <li className="info__item">{article.date}</li>
+        </ul>
+        <div className="actions__container">
+          <button type="button" onClick={() => this.openModalOnEdit(article)} className="actions__btn">Edit</button>
+          <button type="button" onClick={() => this.deleteArticle(article.id)} className="actions__btn">Delete</button>
+        </div>
+
+        <img src={article.imgUrl} alt="Bike" />
+        <div className="content__container">
+          {article.content}
+        </div>
+      </article>
+    ))
+
     return (
-      <div className="">
+      <div className={openModalclassName}>
         <div className="container">
           <nav className="nav">
             <ul className="nav__container">
               <li className="nav__item">
-                <a href="" className="nav__link">Travel updates</a>
+                <a href="/travel" className="nav__link">Travel updates</a>
               </li>
               <li className="nav__item">
-                <a href="" className="nav__link">Reviews</a>
+                <a href="/review" className="nav__link">Reviews</a>
               </li>
               <li className="nav__item">
-                <a href="" className="nav__link">About</a>
+                <a href="/about" className="nav__link">About</a>
               </li>
               <li className="nav__item">
-                <a href="" className="nav__link">Contact</a>
+                <a href="/contact" className="nav__link">Contact</a>
               </li>
             </ul>
           </nav>
           <div className="add__container">
-            <button type="button" className="button"> + Add Article</button>
+            <button type="button" onClick={this.openModalOnAdd} className="button"> + Add Article</button>
           </div>
           <main>
-            <article>
-              <h2 className="title">The complete guide to explore Trasilvania, with your bike</h2>
-              <ul className="info__container">
-                <li className="info__item">Destination Europe</li>
-                <li className="info__item">Added by &nbsp;
-                        <span className="info__mark">Jonnathan Mercadina</span>
-                </li>
-                <li className="info__item">July 01, 2018</li>
-              </ul>
-              <div className="actions__container">
-                <button type="button" className="actions__btn">Edit</button>
-                <button type="button" className="actions__btn">Delete</button>
-              </div>
-
-              <img src="img/bike.jpg" alt="Bike" />
-              <div className="content__container">
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est totam laboriosam debitis magnam voluptatum,
-                incidunt neque. Totam ullam non quis, repellendus molestiae in itaque natus labore quos ipsum alias,
-                veritatis nihil! Quisquam labore, sequi minima expedita necessitatibus omnis error amet recusandae
-                atque commodi quia! Vel laborum recusandae voluptatum rerum id harum, fuga beatae ut, consequuntur
-                        repellendus ipsum temporibus libero itaque.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde quod tempore quaerat deserunt. Voluptatibus
-                possimus, magni quas rem adipisci, esse ipsa fuga, fugit eos repellendus quis? Dicta perferendis,
-                doloremque provident repellendus natus fugit obcaecati, voluptate odio, nulla similique officia.
-                Iure at aliquam dicta provident nulla modi optio maiores. Similique eos molestiae earum voluptatum
-                nostrum porro, consequuntur nihil ex earum. Voluptatum placeat labore necessitatibus repellat. Repudiandae
-                velit suscipit amet tenetur, mollitia aut dolor ipsa delectus a autem ut quibusdam incidunt? Nisi
-                        facilis voluptatem omnis debitis laborum cupiditate pariatur inventore molestiae eveniet!</p>
-              </div>
-            </article>
+            {articleList}
           </main>
           <footer className="footer">
-            <a href="#" className="footer__link">previous</a>
-            <a href="#" className="footer__link">next</a>
+            <a href="/previous" className="footer__link">previous</a>
+            <a href="/next" className="footer__link">next</a>
           </footer>
         </div>
         <div className="modal__overlay">
@@ -66,17 +159,17 @@ class App extends Component {
             <div className="modal__content">
               <h2 className="title">Add/Edit article</h2>
               <div className="inputs__container">
-                <input type="text" className="input" placeholder="Please enter title" />
-                <input type="text" className="input" placeholder="Please enter tag" />
-                <input type="text" className="input" placeholder="Please enter author" />
-                <input type="text" className="input" placeholder="Please enter date" />
-                <input type="text" className="input input--big" placeholder="Please enter image url" />
+                <input type="text" name="title" className="input" value={this.state.title} onChange={this.handleFieldChange} placeholder="Please enter title" />
+                <input type="text" name="tag" className="input" value={this.state.tag} onChange={this.handleFieldChange} placeholder="Please enter tag" />
+                <input type="text" name="author" className="input" value={this.state.author} onChange={this.handleFieldChange} placeholder="Please enter author" />
+                <input type="text" name="date" className="input" value={this.state.date} onChange={this.handleFieldChange} placeholder="Please enter date" />
+                <input type="text" name="imgUrl" className="input input--big" value={this.state.imgUrl} onChange={this.handleFieldChange} placeholder="Please enter image url" />
               </div>
-                <textarea className="textarea" name="content" placeholder="Please enter content"></textarea>
-                <div className="modal__buttons">
-                  <button type="button" className="button">Cancel</button>
-                  <button type="button" className="button button--pink">Save</button>
-                </div>
+              <textarea className="textarea" name="content" value={this.state.content} onChange={this.handleFieldChange} placeholder="Please enter content"></textarea>
+              <div className="modal__buttons">
+                <button type="button" onClick={this.closeModalAndResetArticleState} className="button">Cancel</button>
+                <button type="button" onClick={this.saveArticle} className="button button--pink">Save</button>
+              </div>
             </div>
           </div>
         </div>
